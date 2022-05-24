6f63c40b875612fa8e638efca0a602a599f75100
chango <kimtothechang@gmail.com>
2022-05-25 08:36:29 +0900

    ✨  webpack 실습 테스트 환경 구축

---
+++ src/components/Nodes.js
@@ -0,0 +1,34 @@
+function Nodes({ $app, initialState }) {
+  this.state = initialState;
+  this.$target = document.createElement('ul');
+  this.$target.setAttribute('class', 'Nodes');
+
+  $app.appendChild(this.$target);
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+
+    this.render();
+  };
+
+  this.render = () => {
+    if (this.state.nodes) {
+      const nodesTemplate = this.state.nodes
+        .map((node) => {
+          return `
+          <div class="Node" data-node-id="${node.id}">
+            <img src="./src/img/icon.jpeg" />
+            <div>${node.name}</div>
+          </div>
+        `;
+        })
+        .join('');
+
+      this.$target.innerHTML = nodesTemplate;
+    }
+  };
+
+  this.render();
+}
+
+export default Nodes;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Nodes from './components/Nodes.js';
+
+const $app = document.querySelector('.root');
+
+const initialState = {
+  nodes: [
+    { id: 0, name: '김' },
+    { id: 1, name: '창' },
+    { id: 2, name: '현' },
+  ],
+};
+
+const nodes = new Nodes({ $app, initialState });
